2c2105259b4d44e5f617b578c95d0f6bc11da6e6
Plastic <97719280+PLASTICPEO@users.noreply.github.com>
2023-01-29 15:49:55 +0400

    new Commit

--- src/Components/contents/contents.js
+++ src/Components/contents/contents.js
@@ -1,23 +1,14 @@
-const Contents = ({
-  part1,
-  part2,
-  part3,
-  exercises1,
-  exercises2,
-  exercises3,
-}) => {
-  console.log("new commit");
+const Contents = ({ props }) => {
+  console.log(props);
   return (
     <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      {props.map((item) => {
+        return (
+          <p key={item.name}>
+            {item.name} : {item.exercises}
+          </p>
+        );
+      })}
     </>
   );
 };
--- src/Components/total/total.js
+++ src/Components/total/total.js
@@ -1,5 +1,22 @@
-const Total = ({ exercises1, exercises2, exercises3 }) => {
-  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+const Total = () => {
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
+
+  let result = parts.reduce((a, b) => a.exercises + b.exercises);
+
+  return <p>Number of exercises {result}</p>;
 };
 
 export default Total;
