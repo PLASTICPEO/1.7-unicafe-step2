848f47e87fb1b0ea6922f4634f9c03b934db9115
Plastic <97719280+PLASTICPEO@users.noreply.github.com>
2023-01-31 16:29:38 +0400

    anecdotes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./Exercises-unicafe/App2";
+import App from "./app";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
